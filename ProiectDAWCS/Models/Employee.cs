dfad71c28c824616624e8bcc34acb284957ff030
tudose-bogdan <bogdan-cristian.tudose@my.fmi.unibuc.ro>
2020-12-01 23:19:10 +0200

    Add project files.

---
+++ ProiectDAWCS/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProiectDAWCS.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public int Salary { get; set; }
+        public String JobName { get; set; }
+
+
+
+
+
+    }
+
+    public class HRContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
+
